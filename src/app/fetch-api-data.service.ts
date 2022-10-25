7ef9df2ba7e08cbc0984d2bb1bdfde6442a046d4
gracesuhyun <gracesuhyun@gmail.com>
2022-10-25 00:30:55 -0400

    create user registration form component

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -4,11 +4,6 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-//code that was originally here
-export class FetchApiDataService {
-  constructor() { }
-}
-
 //Declaring the api url that will provide data for the client app
 const apiUrl = 'https://gracean-movies.herokuapp.com/';
 
@@ -16,7 +11,7 @@ const apiUrl = 'https://gracean-movies.herokuapp.com/';
   providedIn: 'root'
 })
 
-export class UserRegistrationService {
+export class FetchApiDataService {
  // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
